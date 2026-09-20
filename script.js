c81baef6fd6782d4b496d92f190268e22055a726
imnotshoshani <LovePowerMagic@proton.me>
2026-09-20 12:59:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('a[href^="#"]').forEach(function(a){
+  a.addEventListener("click",function(e){
+    var target=document.querySelector(a.getAttribute("href"));
+    if(target){e.preventDefault();target.scrollIntoView({behavior:"smooth"});}
+  });
+});
